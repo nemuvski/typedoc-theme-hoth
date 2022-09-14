1629e1d7655e78a4d13d17ac9aeb1a8a19d6a978
K.Utsunomiya <kii.utsunomiya@gmail.com>
2022-09-14 14:57:37 +0900

    Fix: closing dropdown menu

--- theme/scripts/dropdown.ts
+++ theme/scripts/dropdown.ts
@@ -1,6 +1,28 @@
 import { JSHOOK_DROPDOWN_BUTTON, JSHOOK_DROPDOWN_CONTENT } from '../../src/constants'
 
+function closeAllDropdown() {
+  document.querySelectorAll(`.is-open[data-jshook='${JSHOOK_DROPDOWN_CONTENT}']`).forEach((elm) => {
+    elm.classList.remove('is-open')
+  })
+}
+
 export function setEventDropdown() {
+  /**
+   * Closing dropdown by clicking outside of dropdown menu
+   */
+  document.documentElement.addEventListener('click', (e) => {
+    const currentTarget = e.currentTarget
+    if (!(currentTarget instanceof HTMLElement)) {
+      return
+    }
+    if (!currentTarget.matches(`[data-jshook='${JSHOOK_DROPDOWN_CONTENT}']`)) {
+      closeAllDropdown()
+    }
+  })
+
+  /**
+   * Open dropdown menu
+   */
   document.querySelectorAll(`button[data-jshook='${JSHOOK_DROPDOWN_BUTTON}']`).forEach((elm) => {
     elm.addEventListener(
       'click',
@@ -10,6 +32,8 @@ export function setEventDropdown() {
           return
         }
 
+        e.stopPropagation()
+
         const contentElm = currentTarget.parentElement?.querySelector(`[data-jshook='${JSHOOK_DROPDOWN_CONTENT}']`)
         if (!contentElm) {
           return
@@ -18,10 +42,20 @@ export function setEventDropdown() {
         if (isOpen) {
           contentElm.classList.remove('is-open')
         } else {
+          closeAllDropdown()
           contentElm.classList.add('is-open')
         }
       },
       {}
     )
+  })
+
+  /**
+   * Ensure that the menu does not close when an item in a drop-down menu is clicked
+   */
+  document.querySelectorAll(`[data-jshook='${JSHOOK_DROPDOWN_CONTENT}']`).forEach((elm) => {
+    elm.addEventListener('click', (e) => {
+      e.stopPropagation()
+    })
   })
 }
